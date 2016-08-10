b75cb55a32580153d7c4ddbd485940d9f035b137
Jorge Y. C. Rodriguez <developer@jorgerodriguez.dk>
2016-08-10 19:27:50 +0200

    adding more test

--- tests/AnnualReportServiceTest.php
+++ tests/AnnualReportServiceTest.php
@@ -8,6 +8,11 @@ use Dotenv\Dotenv;
 
 class AnnualReportServiceTest extends TestCase
 {
+    /**
+     * @var AnnualReportService
+     */
+    private $service;
+
     protected function setUp()
     {
         parent::setUp();
@@ -15,13 +20,12 @@ class AnnualReportServiceTest extends TestCase
         if (is_readable($baseDir . '/.env')) {
             (new Dotenv($baseDir))->load();
         }
+        $this->service = new AnnualReportService();
     }
 
     public function test_if_single_annual_report_is_returned()
     {
-        $service = new AnnualReportService();
-
-        $company = $service->latestAnnualReport('33966369', 'dk');
+        $company = $this->service->latestAnnualReport('33966369', 'dk');
         $this->assertEquals(AnnualReport::class, get_class($company));
     }
 
@@ -30,30 +34,39 @@ class AnnualReportServiceTest extends TestCase
      */
     public function test_that_empty_result_exception_is_thrown()
     {
-        $service = new AnnualReportService();
-        $service->latestAnnualReportByVatNumberOrFail('111000', 'dk');
+        $this->service->latestAnnualReportByVatNumberOrFail('111000', 'dk');
     }
 
     public function test_that_no_providers_are_returned_when_geo_code_is_empty()
     {
-        $service = new AnnualReportService();
-        $providers = $service->providers('');
-
+        $providers = $this->service->providers('');
         $this->assertEmpty($providers);
     }
 
     public function test_document_properties()
     {
-        $service = new AnnualReportService();
-        $ar = $service->latestAnnualReport('33966369', 'dk');
+        $ar = $this->service->latestAnnualReport('33966369', 'dk');
         $this->assertEquals(AnnualReport::SUPPORTED_FORMAT, $ar->document_mime_type);
     }
 
     public function test_if_provider_exist()
     {
-        $service = new AnnualReportService();
-
-        $providers = $service->providers('dk');
+        $providers = $this->service->providers('dk');
         $this->assertNotEmpty($providers);
+    }
+
+    public function test_annual_report_by_vat_number()
+    {
+        $company_reports = $this->service->annualReportsByVatNumber('33966369', 'dk');
+        $first_report = $company_reports[0];
+        $this->assertEquals(AnnualReport::class, get_class($first_report));
+        $this->assertEquals(!empty($company_reports), true);
+        $this->assertEquals(is_array($company_reports), true);
+    }
+
+    public function test_annual_report_or_fail_return_right_value()
+    {
+        $company_reports = $this->service->latestAnnualReportByVatNumberOrFail('33966369', 'dk');
+        $this->assertEquals(AnnualReport::SUPPORTED_FORMAT, $company_reports->document_mime_type);
     }
 }
